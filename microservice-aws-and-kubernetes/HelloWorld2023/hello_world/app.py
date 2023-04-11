f6c29fa08d1c233472f0e2d2cb2b1b234bd0ac11
Đổng Giang Thái <donggiangthai1998@gmail.com>
2023-04-12 04:28:03 +0700

    Exercise: Making Change

--- microservice-aws-and-kubernetes/HelloWorld2023/hello_world/app.py
+++ microservice-aws-and-kubernetes/HelloWorld2023/hello_world/app.py
@@ -1,33 +1,50 @@
+# imported libraries
 import json
+import decimal
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    ''' Accepts some US dollar input and returns the amount of change
+        needed to make up that value.
+        :param event: a request that has a JSON input with some "amount" value
+        :return: a response with the correct US change to make up the amount,
+            formatted in JSON as the number of quarters/dimes/nickels/pennies.
+    '''
+    
+    print(event)
+    
+    # check that the request has some input body
+    if 'body' in event:
+        event = json.loads(event["body"])
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    # get float "amount"
+    amount = float(event["amount"])
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    # calculate the resultant change and store the result (res)
+    res = []
+    coins = [1,5,10,25] # value of pennies, nickels, dimes, quarters
+    coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+    # divide the amount*100 (the amount in cents) by a coin value
+    # record the number of coins that evenly divide and the remainder
+    coin = coins.pop()
+    num, rem  = divmod(int(amount*100), coin)
+    # append the coin type and number of coins that had no remainder
+    res.append({num:coin_lookup[coin]})
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    # while there is still some remainder, continue adding coins to the result
+    while rem > 0:
+        coin = coins.pop()
+        num, rem = divmod(rem, coin)
+        if num:
+            if coin in coin_lookup:
+                res.append({num:coin_lookup[coin]})
 
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-            }
-        ),
+    # format the response as JSON and return the result
+    response = {
+        "statusCode": "200",
+        "headers": { "Content-type": "application/json" },
+        "body": json.dumps({"res": res})
     }
+
+    return response
