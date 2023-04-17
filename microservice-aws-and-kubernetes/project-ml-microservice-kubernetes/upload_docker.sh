06c68e61c26b3425ee9ebb10ba5ab77915a642be
Đổng Giang Thái <donggiangthai1998@gmail.com>
2023-04-18 05:46:01 +0700

    Complete upload_docker.sh

--- microservice-aws-and-kubernetes/project-ml-microservice-kubernetes/upload_docker.sh
+++ microservice-aws-and-kubernetes/project-ml-microservice-kubernetes/upload_docker.sh
@@ -4,12 +4,28 @@
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+# Create docker_path
+docker_path=donggiangthai
+source .env
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $docker_path"
+echo "$MY_PASSWORD" | docker login --username $docker_path --password-stdin
+image_tagged=$(docker image list --filter=reference="$docker_path/project-ml" | grep 'project-ml' | xargs)
+if [[ -n $image_tagged ]]; then
+  echo "Image already tagged, remove the tagged image."
+  name=$(echo "$image_tagged" | cut -f 1 -d " ")
+  tag=$(echo "$image_tagged" | cut -f 2 -d " ")
+  docker image remove --force "$name":"$tag"
+fi
+
+image_info=$(docker image list | grep 'project-ml' | xargs)
+image_name=$(echo "$image_info" | cut -f 1 -d " ")
+image_tag=$(echo "$image_info" | cut -f 2 -d " ")
+docker image tag "$image_name:$image_tag" "$docker_path/$image_name:$image_tag"
+docker image list --filter=reference="$docker_path/project-ml"
 
 # Step 3:
 # Push image to a docker repository
+docker image push "$docker_path/project-ml:$image_tag"
