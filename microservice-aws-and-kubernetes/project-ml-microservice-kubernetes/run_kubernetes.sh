02d5513e6b4400043732b5e8e0b12abfdb04668d
Đổng Giang Thái <donggiangthai1998@gmail.com>
2023-04-18 05:46:54 +0700

    Complete run_kubernetes.sh

--- microservice-aws-and-kubernetes/project-ml-microservice-kubernetes/run_kubernetes.sh
+++ microservice-aws-and-kubernetes/project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+docker_path=donggiangthai
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy project-ml-microservice-kubernetes --image="$docker_path/project-ml:v1.0.0"
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment.apps/project-ml-microservice-kubernetes 8000:80
