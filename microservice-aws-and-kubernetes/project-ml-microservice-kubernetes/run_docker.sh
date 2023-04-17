af542217f91c97c7e4ca79a87dfa1638f0f8b67c
Đổng Giang Thái <donggiangthai1998@gmail.com>
2023-04-18 05:45:46 +0700

    Complete run_docker.sh

--- microservice-aws-and-kubernetes/project-ml-microservice-kubernetes/run_docker.sh
+++ microservice-aws-and-kubernetes/project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag project-ml:v1.0.0 .
 
 # Step 2: 
 # List docker images
+docker image list
 
-# Step 3: 
+# Step 3:
 # Run flask app
+docker run --detach --publish 8000:80 project-ml:v1.0.0
